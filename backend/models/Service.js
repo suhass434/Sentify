97b25fdddfb6aa0ef4bdbf6953e3ec76c6c7163e
Abhishek V K <117388178+Abhishekvk04@users.noreply.github.com>
2024-12-22 15:01:46 +0530

    changed backend directory

--- backend/models/Service.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const serviceSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-});
-
-const Service = mongoose.model('Service', serviceSchema);
-
-export default Service;
