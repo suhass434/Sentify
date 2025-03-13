b61051b1e775f1907ddfc729d2c13a35f781eae2
suhass434 <1ms22is139@msrit.edu>
2025-03-13 23:22:47 +0530

    Downgrade python

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Install Python 3.13.2 (if supported)
-pyenv install 3.13.2 --skip-existing
-pyenv global 3.13.2
+pyenv install 3.10.12 --skip-existing
+pyenv global 3.10.12
 
 # Install dependencies
 pip install -r requirements.txt
