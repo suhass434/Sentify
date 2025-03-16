44965492a3dc24dd1413d72be4e518dd1263baed
suhass434 <1ms22is139@msrit.edu>
2025-03-16 19:24:36 +0530

    Reduce fetch time

--- backend/render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Install Python 3.13.2 (if supported)
-pyenv install 3.10.12 --skip-existing
-pyenv global 3.10.12
-
-# Install dependencies
-pip install -r requirements.txt
-npm install
